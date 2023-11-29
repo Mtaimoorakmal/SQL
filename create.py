da98899ab58bef2be10dfb53f31c1449de024eef
Mtaimoorakmal <94taimoor@gmail.com>
2023-11-29 16:54:17 +0500

    created sample.db database

--- create.py
+++ create.py
@@ -1,4 +1,9 @@
 print("hello world")
 import sqlite3
 
-connection = sqlite3.connect()
+connection = sqlite3.connect("sample.db")
+
+table = 'CREATE TABLE People (id integer primary key , name TEXT , surmane TEXT)'
+courser = connection.cursor()
+courser.execute(table)
+connection.commit()
